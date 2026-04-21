b334e71fa37b9794b36140869280ad98faad6bb1
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2026-04-21 17:23:12 -0300

    Service de clientes

---
+++ src/app/cliente.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from './cadastro/cliente';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClienteService {
+
+  static REPO_CLIENTES = "_CLIENTES";
+  private storage: Storage = localStorage
+
+  constructor() {}
+
+  salvar(cliente: Cliente) {
+    const clientes = this.obterStorage()
+    clientes.push(cliente)
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes))
+  }
+
+  obterStorage(): Cliente[] {
+    const repositorioClientes = this.storage.getItem(ClienteService.REPO_CLIENTES)
+    if (repositorioClientes) {
+      const clientes: Cliente[] = JSON.parse(repositorioClientes)
+      return clientes
+    }
+    
+    const clientes: Cliente[] = [];
+    this.storage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes))
+    return clientes
+  }
+}
